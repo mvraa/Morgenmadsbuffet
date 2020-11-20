354a1d8768a80e7b8f6ebcf0b5e0d9ac2bc57826
mvraa <40834090+mvraa@users.noreply.github.com>
2020-11-20 08:45:40 +0100

    Add project files.

---
+++ Morgenmadsbuffet/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Morgenmadsbuffet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
